101b3d0f7584f8d9a8ec38bcd3701a9d05ae2b27
reokadhafi <106902195+reokadhafi@users.noreply.github.com>
2024-12-01 16:02:27 +0900

    Update add-akun.sh

--- menu/add-akun.sh
+++ menu/add-akun.sh
@@ -71,6 +71,7 @@ sed -i '/#vmess$/a\### '"$user $exp"'\
 sed -i '/#vmessgrpc$/a\### '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
 
+#buatvmess
 VMESSWS_JSON=$(jq -n \
     --arg uuid "$uuid" \
     --arg email "$user" \
@@ -116,6 +117,7 @@ VMESSWS_B64=$(echo -n $VMESSWS_JSON | base64 -w 0)
 VMESSGRPC_B64=$(echo -n $VMESSGRPC_JSON | base64 -w 0)
 
 systemctl restart xray
+
 #buatvless
 vlesslinkws="vless://${uuid}@${domain}:443?path=/xrayws&security=tls&encryption=none&type=ws#${user}"
 vlesslinknon="vless://${uuid}@${domain}:80?path=/xrayws&encryption=none&type=ws#${user}"
@@ -124,237 +126,237 @@ vlesslinkgrpc="vless://${uuid}@${domain}:443?mode=gun&security=tls&encryption=no
 #buattrojan
 trojanlinkgrpc="trojan://${uuid}@${domain}:443?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=bug.com#${user}"
 trojanlinkws="trojan://${uuid}@${domain}:443?path=/xraytrojanws&security=tls&host=bug.com&type=ws&sni=bug.com#${user}"
+
 #buatshadowsocks
-#
-cipher="aes-128-gcm"
-sed -i '/#ssws$/a\### '"$user $exp"'\
-},{"password": "'""$uuid""'","method": "'""$cipher""'","email": "'""$user""'"' /etc/xray/config.json
-sed -i '/#ssgrpc$/a\### '"$user $exp"'\
-},{"password": "'""$uuid""'","method": "'""$cipher""'","email": "'""$user""'"' /etc/xray/config.json
-echo $cipher:$uuid > /tmp/log
-shadowsocks_base64=$(cat /tmp/log)
-echo -n "${shadowsocks_base64}" | base64 > /tmp/log1
-shadowsocks_base64e=$(cat /tmp/log1)
-shadowsockslink="ss://${shadowsocks_base64e}@$domain:$tls?plugin=xray-plugin;mux=0;path=/xrayssws;host=$domain;tls#${user}"
-shadowsockslink1="ss://${shadowsocks_base64e}@$domain:$tls?plugin=xray-plugin;mux=0;serviceName=ss-grpc;host=$domain;tls#${user}"
+# cipher="aes-128-gcm"
+# sed -i '/#ssws$/a\### '"$user $exp"'\
+# },{"password": "'""$uuid""'","method": "'""$cipher""'","email": "'""$user""'"' /etc/xray/config.json
+# sed -i '/#ssgrpc$/a\### '"$user $exp"'\
+# },{"password": "'""$uuid""'","method": "'""$cipher""'","email": "'""$user""'"' /etc/xray/config.json
+# echo $cipher:$uuid > /tmp/log
+# shadowsocks_base64=$(cat /tmp/log)
+# echo -n "${shadowsocks_base64}" | base64 > /tmp/log1
+# shadowsocks_base64e=$(cat /tmp/log1)
+# shadowsockslink="ss://${shadowsocks_base64e}@$domain:$tls?plugin=xray-plugin;mux=0;path=/xrayssws;host=$domain;tls#${user}"
+# shadowsockslink1="ss://${shadowsocks_base64e}@$domain:$tls?plugin=xray-plugin;mux=0;serviceName=ss-grpc;host=$domain;tls#${user}"
+
 systemctl restart xray
 rm -rf /tmp/log
 rm -rf /tmp/log1
-cat > /home/vps/public_html/ss-ws-$user.txt <<-END
-{ 
- "dns": {
-    "servers": [
-      "8.8.8.8",
-      "8.8.4.4"
-    ]
-  },
- "inbounds": [
-   {
-      "port": 10808,
-      "protocol": "socks",
-      "settings": {
-        "auth": "noauth",
-        "udp": true,
-        "userLevel": 8
-      },
-      "sniffing": {
-        "destOverride": [
-          "http",
-          "tls"
-        ],
-        "enabled": true
-      },
-      "tag": "socks"
-    },
-    {
-      "port": 10809,
-      "protocol": "http",
-      "settings": {
-        "userLevel": 8
-      },
-      "tag": "http"
-    }
-  ],
-  "log": {
-    "loglevel": "none"
-  },
-  "outbounds": [
-    {
-      "mux": {
-        "enabled": true
-      },
-      "protocol": "shadowsocks",
-      "settings": {
-        "servers": [
-          {
-            "address": "$domain",
-            "level": 8,
-            "method": "$cipher",
-            "password": "$uuid",
-            "port": 443
-          }
-        ]
-      },
-      "streamSettings": {
-        "network": "ws",
-        "security": "tls",
-        "tlsSettings": {
-          "allowInsecure": true,
-          "serverName": "isi_bug_disini"
-        },
-        "wsSettings": {
-          "headers": {
-            "Host": "$domain"
-          },
-          "path": "/xrayssws"
-        }
-      },
-      "tag": "proxy"
-    },
-    {
-      "protocol": "freedom",
-      "settings": {},
-      "tag": "direct"
-    },
-    {
-      "protocol": "blackhole",
-      "settings": {
-        "response": {
-          "type": "http"
-        }
-      },
-      "tag": "block"
-    }
-  ],
-  "policy": {
-    "levels": {
-      "8": {
-        "connIdle": 300,
-        "downlinkOnly": 1,
-        "handshake": 4,
-        "uplinkOnly": 1
-      }
-    },
-    "system": {
-      "statsOutboundUplink": true,
-      "statsOutboundDownlink": true
-    }
-  },
-  "routing": {
-    "domainStrategy": "Asls",
-"rules": []
-  },
-  "stats": {}
-}
-END
-cat > /home/vps/public_html/ss-grpc-$user.txt <<-END
-{
-    "dns": {
-    "servers": [
-      "8.8.8.8",
-      "8.8.4.4"
-    ]
-  },
- "inbounds": [
-   {
-      "port": 10808,
-      "protocol": "socks",
-      "settings": {
-        "auth": "noauth",
-        "udp": true,
-        "userLevel": 8
-      },
-      "sniffing": {
-        "destOverride": [
-          "http",
-          "tls"
-        ],
-        "enabled": true
-      },
-      "tag": "socks"
-    },
-    {
-      "port": 10809,
-      "protocol": "http",
-      "settings": {
-        "userLevel": 8
-      },
-      "tag": "http"
-    }
-  ],
-  "log": {
-    "loglevel": "none"
-  },
-  "outbounds": [
-    {
-      "mux": {
-        "enabled": true
-      },
-      "protocol": "shadowsocks",
-      "settings": {
-        "servers": [
-          {
-            "address": "$domain",
-            "level": 8,
-            "method": "$cipher",
-            "password": "$uuid",
-            "port": 443
-          }
-        ]
-      },
-      "streamSettings": {
-        "grpcSettings": {
-          "multiMode": true,
-          "serviceName": "ss-grpc"
-        },
-        "network": "grpc",
-        "security": "tls",
-        "tlsSettings": {
-          "allowInsecure": true,
-          "serverName": "isi_bug_disini"
-        }
-      },
-      "tag": "proxy"
-    },
-    {
-      "protocol": "freedom",
-      "settings": {},
-      "tag": "direct"
-    },
-    {
-      "protocol": "blackhole",
-      "settings": {
-        "response": {
-          "type": "http"
-        }
-      },
-      "tag": "block"
-    }
-  ],
-  "policy": {
-    "levels": {
-      "8": {
-        "connIdle": 300,
-        "downlinkOnly": 1,
-        "handshake": 4,
-        "uplinkOnly": 1
-      }
-    },
-    "system": {
-      "statsOutboundUplink": true,
-      "statsOutboundDownlink": true
-    }
-  },
-  "routing": {
-    "domainStrategy": "Asls",
-"rules": []
-  },
-  "stats": {}
-}
-END
 
-#
-#buatvmess
+# cat > /home/vps/public_html/ss-ws-$user.txt <<-END
+# { 
+#  "dns": {
+#     "servers": [
+#       "8.8.8.8",
+#       "8.8.4.4"
+#     ]
+#   },
+#  "inbounds": [
+#    {
+#       "port": 10808,
+#       "protocol": "socks",
+#       "settings": {
+#         "auth": "noauth",
+#         "udp": true,
+#         "userLevel": 8
+#       },
+#       "sniffing": {
+#         "destOverride": [
+#           "http",
+#           "tls"
+#         ],
+#         "enabled": true
+#       },
+#       "tag": "socks"
+#     },
+#     {
+#       "port": 10809,
+#       "protocol": "http",
+#       "settings": {
+#         "userLevel": 8
+#       },
+#       "tag": "http"
+#     }
+#   ],
+#   "log": {
+#     "loglevel": "none"
+#   },
+#   "outbounds": [
+#     {
+#       "mux": {
+#         "enabled": true
+#       },
+#       "protocol": "shadowsocks",
+#       "settings": {
+#         "servers": [
+#           {
+#             "address": "$domain",
+#             "level": 8,
+#             "method": "$cipher",
+#             "password": "$uuid",
+#             "port": 443
+#           }
+#         ]
+#       },
+#       "streamSettings": {
+#         "network": "ws",
+#         "security": "tls",
+#         "tlsSettings": {
+#           "allowInsecure": true,
+#           "serverName": "isi_bug_disini"
+#         },
+#         "wsSettings": {
+#           "headers": {
+#             "Host": "$domain"
+#           },
+#           "path": "/xrayssws"
+#         }
+#       },
+#       "tag": "proxy"
+#     },
+#     {
+#       "protocol": "freedom",
+#       "settings": {},
+#       "tag": "direct"
+#     },
+#     {
+#       "protocol": "blackhole",
+#       "settings": {
+#         "response": {
+#           "type": "http"
+#         }
+#       },
+#       "tag": "block"
+#     }
+#   ],
+#   "policy": {
+#     "levels": {
+#       "8": {
+#         "connIdle": 300,
+#         "downlinkOnly": 1,
+#         "handshake": 4,
+#         "uplinkOnly": 1
+#       }
+#     },
+#     "system": {
+#       "statsOutboundUplink": true,
+#       "statsOutboundDownlink": true
+#     }
+#   },
+#   "routing": {
+#     "domainStrategy": "Asls",
+# "rules": []
+#   },
+#   "stats": {}
+# }
+# END
+# cat > /home/vps/public_html/ss-grpc-$user.txt <<-END
+# {
+#     "dns": {
+#     "servers": [
+#       "8.8.8.8",
+#       "8.8.4.4"
+#     ]
+#   },
+#  "inbounds": [
+#    {
+#       "port": 10808,
+#       "protocol": "socks",
+#       "settings": {
+#         "auth": "noauth",
+#         "udp": true,
+#         "userLevel": 8
+#       },
+#       "sniffing": {
+#         "destOverride": [
+#           "http",
+#           "tls"
+#         ],
+#         "enabled": true
+#       },
+#       "tag": "socks"
+#     },
+#     {
+#       "port": 10809,
+#       "protocol": "http",
+#       "settings": {
+#         "userLevel": 8
+#       },
+#       "tag": "http"
+#     }
+#   ],
+#   "log": {
+#     "loglevel": "none"
+#   },
+#   "outbounds": [
+#     {
+#       "mux": {
+#         "enabled": true
+#       },
+#       "protocol": "shadowsocks",
+#       "settings": {
+#         "servers": [
+#           {
+#             "address": "$domain",
+#             "level": 8,
+#             "method": "$cipher",
+#             "password": "$uuid",
+#             "port": 443
+#           }
+#         ]
+#       },
+#       "streamSettings": {
+#         "grpcSettings": {
+#           "multiMode": true,
+#           "serviceName": "ss-grpc"
+#         },
+#         "network": "grpc",
+#         "security": "tls",
+#         "tlsSettings": {
+#           "allowInsecure": true,
+#           "serverName": "isi_bug_disini"
+#         }
+#       },
+#       "tag": "proxy"
+#     },
+#     {
+#       "protocol": "freedom",
+#       "settings": {},
+#       "tag": "direct"
+#     },
+#     {
+#       "protocol": "blackhole",
+#       "settings": {
+#         "response": {
+#           "type": "http"
+#         }
+#       },
+#       "tag": "block"
+#     }
+#   ],
+#   "policy": {
+#     "levels": {
+#       "8": {
+#         "connIdle": 300,
+#         "downlinkOnly": 1,
+#         "handshake": 4,
+#         "uplinkOnly": 1
+#       }
+#     },
+#     "system": {
+#       "statsOutboundUplink": true,
+#       "statsOutboundDownlink": true
+#     }
+#   },
+#   "routing": {
+#     "domainStrategy": "Asls",
+# "rules": []
+#   },
+#   "stats": {}
+# }
+# END
+
 clear
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
 echo -e "====== XRAY MANTAP Multi Port=======" | tee -a /etc/log-create-user.log
@@ -378,22 +380,25 @@ echo -e "=> GRPC   : trojan-grpc" | tee -a /etc/log-create-user.log
 echo -e "=> OPOK   : ws://bugcom/xraytrojanws" | tee -a /etc/log-create-user.log
 echo -e "====== Trojan WS =======" | tee -a /etc/log-create-user.log
 echo -e "$trojanlinkws" | tee -a /etc/log-create-user.log
+echo -e "========================" | tee -a /etc/log-create-user.log
 echo -e "====== Trojan GRPC =======" | tee -a /etc/log-create-user.log
 echo -e "$trojanlinkgrpc" | tee -a /etc/log-create-user.log
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
+echo -e "========================" | tee -a /etc/log-create-user.log
+echo -e "" | tee -a /etc/log-create-user.log
 
-echo -e "Protokol VPN: SHADOWSOCKS" | tee -a /etc/log-create-user.log
-echo -e "Network: WS/GRPC" | tee -a /etc/log-create-user.log
-echo -e "Method Cipers : aes-128-gcm" | tee -a /etc/log-create-user.log
-echo -e "====== Path =======" | tee -a /etc/log-create-user.log
-echo -e "=> WS TLS : /xrayssws" | tee -a /etc/log-create-user.log
-echo -e "=> GRPC   : ss-grpc" | tee -a /etc/log-create-user.log
-echo -e "=> OPOK   : ws://bugcom/xrayssws" | tee -a /etc/log-create-user.log
-echo -e "====== SHADOWSOCKS WS TLS=======" | tee -a /etc/log-create-user.log
-echo -e "URL Custom Config WS TLS   :http://${domain}:89/ss-ws-$user.txt" | tee -a /etc/log-create-user.log
-echo -e "====== SHADOWSOCKS GRPC TLS=======" | tee -a /etc/log-create-user.log
-echo -e "URL Custom Config GRPC TLS : http://${domain}:89/ss-grpc-$user.txt" | tee -a /etc/log-create-user.log
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
+# echo -e "Protokol VPN: SHADOWSOCKS" | tee -a /etc/log-create-user.log
+# echo -e "Network: WS/GRPC" | tee -a /etc/log-create-user.log
+# echo -e "Method Cipers : aes-128-gcm" | tee -a /etc/log-create-user.log
+# echo -e "====== Path =======" | tee -a /etc/log-create-user.log
+# echo -e "=> WS TLS : /xrayssws" | tee -a /etc/log-create-user.log
+# echo -e "=> GRPC   : ss-grpc" | tee -a /etc/log-create-user.log
+# echo -e "=> OPOK   : ws://bugcom/xrayssws" | tee -a /etc/log-create-user.log
+# echo -e "====== SHADOWSOCKS WS TLS=======" | tee -a /etc/log-create-user.log
+# echo -e "URL Custom Config WS TLS   :http://${domain}:89/ss-ws-$user.txt" | tee -a /etc/log-create-user.log
+# echo -e "====== SHADOWSOCKS GRPC TLS=======" | tee -a /etc/log-create-user.log
+# echo -e "URL Custom Config GRPC TLS : http://${domain}:89/ss-grpc-$user.txt" | tee -a /etc/log-create-user.log
+# echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
+# echo -e "" | tee -a /etc/log-create-user.log
 
 echo -e "Protokol VPN: VLESS" | tee -a /etc/log-create-user.log
 echo -e "Network: WS/GRPC" | tee -a /etc/log-create-user.log
@@ -401,11 +406,15 @@ echo -e "====== Path =======" | tee -a /etc/log-create-user.log
 echo -e "=> WS TLS : /xrayws" | tee -a /etc/log-create-user.log
 echo -e "=> GRPC   : vless-grpc" | tee -a /etc/log-create-user.log
 echo -e "=> OPOK   : ws://bugcom/xrayws" | tee -a /etc/log-create-user.log
-echo -e "====== Vless WS TLS =======" | tee -a /etc/log-create-user.log
-echo -e "Link Config WS TLS    : $vlesslinkws" | tee -a /etc/log-create-user.log
+echo -e "======= Vless WS NO TLS ========" | tee -a /etc/log-create-user.log
+echo -e "$vlesslinknon" | tee -a /etc/log-create-user.log
+echo -e "======= Vless WS TLS ========" | tee -a /etc/log-create-user.log
+echo -e "$vlesslinkws" | tee -a /etc/log-create-user.log
+echo -e "=============================" | tee -a /etc/log-create-user.log
 echo -e "====== Vless GRPC TLS =======" | tee -a /etc/log-create-user.log
-echo -e "Link Config GRPC TLS  : $vlesslinkgrpc" | tee -a /etc/log-create-user.log
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
+echo -e "$vlesslinkgrpc" | tee -a /etc/log-create-user.log
+echo -e "=============================" | tee -a /etc/log-create-user.log
+echo -e "" | tee -a /etc/log-create-user.log
 
 echo -e "Protokol VPN: VMESS" | tee -a /etc/log-create-user.log
 echo -e "Alter ID: 0" | tee -a /etc/log-create-user.log
@@ -414,11 +423,12 @@ echo -e "====== Path =======" | tee -a /etc/log-create-user.log
 echo -e "=> WS TLS : /xrayvws" | tee -a /etc/log-create-user.log
 echo -e "=> GRPC   : vmess-grpc" | tee -a /etc/log-create-user.log
 echo -e "=> OPOK   : ws://bugcom/xrayvws" | tee -a /etc/log-create-user.log
-echo -e "====== Vmess WS =======" | tee -a /etc/log-create-user.log
+echo -e "======= Vmess WS ========" | tee -a /etc/log-create-user.log
 echo -e "vmess://$VMESSWS_B64" | tee -a /etc/log-create-user.log
+echo -e "=========================" | tee -a /etc/log-create-user.log
 echo -e "====== Vmess GRPC =======" | tee -a /etc/log-create-user.log
 echo -e "vmess://$VMESSGRPC_B64" | tee -a /etc/log-create-user.log
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
-echo -e "SCRIPT MANTAP XRAY" | tee -a /etc/log-create-user.log
+echo -e "=========================" | tee -a /etc/log-create-user.log
+echo -e "SCRIPT XRAY-MOD" | tee -a /etc/log-create-user.log
 echo "" | tee -a /etc/log-create-user.log
 cd
