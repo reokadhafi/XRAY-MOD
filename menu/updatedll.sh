656df0d916d7bf4afc034abd6a415cfe15c4d463
reokadhafi <106902195+reokadhafi@users.noreply.github.com>
2024-12-01 12:32:44 +0900

    Update updatedll.sh

--- menu/updatedll.sh
+++ menu/updatedll.sh
@@ -18,7 +18,7 @@ wget -O add-akun "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/men
 wget -O updatedll "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/menu/updatedll.sh"
 wget -O add-akun "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/menu/add-akun.sh"
 wget -O delete-akun "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/menu/delete-akun.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/xray/certv2ray.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/xray/certv2ray.sh"
 wget -O restart-xray "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/menu/restart-xray.sh"
 wget -O xmenu "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/menu/xmenu.sh"
 wget -O auto-pointing "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/menu/auto-pointing.sh"
