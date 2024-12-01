c2a0c33076a34641142e772886ccbebfb853c236
reokadhafi <106902195+reokadhafi@users.noreply.github.com>
2024-12-01 14:25:27 +0900

    Update install-tools.sh

--- install-tools.sh
+++ install-tools.sh
@@ -127,7 +127,8 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 curl https://${akbarvpn}/nginx.conf > /etc/nginx/nginx.conf
 curl https://${akbarvpn}/vps.conf > /etc/nginx/conf.d/vps.conf
-sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
+#sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
+sed -i 's|listen = /var/run/php/php8.1-fpm.sock|listen = 127.0.0.1:9000|g' /etc/php/8.1/fpm/pool.d/www.conf
 useradd -m vps;
 mkdir -p /home/vps/public_html
 echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
