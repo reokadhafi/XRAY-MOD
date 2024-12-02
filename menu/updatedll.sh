eaf93c22be115277e6b90f716221d1f5f3378819
reokadhafi <106902195+reokadhafi@users.noreply.github.com>
2024-12-03 01:39:57 +0900

    Update updatedll.sh

--- menu/updatedll.sh
+++ menu/updatedll.sh
@@ -9,13 +9,15 @@ wget -O xmenu "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/menu/x
 wget -O add-akun "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/menu/add-akun.sh"
 wget -O updatedll "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/menu/updatedll.sh"
 wget -O delete-akun "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/menu/delete-akun.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/xray/certv2ray.sh"
 wget -O restart-xray "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/menu/restart-xray.sh"
 wget -O auto-pointing "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/menu/auto-pointing.sh"
 wget -O cek-port "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/menu/cek-port.sh"
 wget -O auto-delete-expired "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/menu/auto-delete-expired.sh"
 
+# wget -O certv2ray "https://raw.githubusercontent.com/reokadhafi/XRAY-MOD/main/xray/certv2ray.sh"
+# chmod +x /usr/bin/certv2ray
+
 # Berikan izin eksekusi
-chmod +x /usr/bin/updatedll /usr/bin/xmenu /usr/bin/add-akun /usr/bin/delete-akun /usr/bin/certv2ray /usr/bin/restart-xray /usr/bin/auto-pointing /usr/bin/cek-port /usr/bin/auto-delete-expired
+chmod +x /usr/bin/updatedll /usr/bin/xmenu /usr/bin/add-akun /usr/bin/delete-akun /usr/bin/restart-xray /usr/bin/auto-pointing /usr/bin/cek-port /usr/bin/auto-delete-expired
 
 cd
