68098a81ca1295feae7ad9027dd7af5f487ed47e
reokadhafi <106902195+reokadhafi@users.noreply.github.com>
2024-12-02 11:27:33 +0900

    Update xmenu.sh

--- menu/xmenu.sh
+++ menu/xmenu.sh
@@ -60,7 +60,38 @@ curl -s ipinfo.io/ip
 updatedll
 ;;
 6)
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --without-geodata
+systemctl stop nginx
+rm -rf /etc/systemd/system/xray.service.d
+cat <<EOF> /etc/systemd/system/xray.service
+Description=Xray Service
+Documentation=https://github.com/xtls
+After=network.target nss-lookup.target
+
+[Service]
+User=www-data
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/local/bin/xray run -config /etc/xray/config.json
+Restart=on-failure
+RestartPreventExitStatus=23
+LimitNPROC=10000
+LimitNOFILE=1000000
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+systemctl daemon-reload
+sleep 1
+systemctl enable xray
+systemctl restart xray
+systemctl restart nginx
+systemctl enable runn
+systemctl restart runn
+echo "XRAY Core & Geo Update Success"
 ;;
 7)
 restart-xray
