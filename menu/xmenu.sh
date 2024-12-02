3af1dbe1e61e062764c8619a076d0441f7b5f49e
reokadhafi <106902195+reokadhafi@users.noreply.github.com>
2024-12-02 11:04:32 +0900

    Update xmenu.sh

--- menu/xmenu.sh
+++ menu/xmenu.sh
@@ -61,6 +61,27 @@ updatedll
 ;;
 6)
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --without-geodata
+rm -rf /etc/systemd/system/xray.service.d
+cat <<EOF> /etc/systemd/system/xray.service
+Description=Xray Service
+Documentation=https://github.com/xtls
+After=network.target nss-lookup.target
+
+[Service]
+User=www-data
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/local/bin/xray run -config /etc/xray/config.json
+Restart=on-failure
+RestartPreventExitStatus=23
+LimitNPROC=10000
+LimitNOFILE=1000000
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
 ;;
 7)
 restart-xray
